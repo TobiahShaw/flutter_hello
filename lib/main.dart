89ae32e907e47533ecac8199bfa53a66a2de7695
TobiahShaw <a5414994@outlook.com>
2019-04-29 08:08:22 +0800

    usage of tab

--- lib/main.dart
+++ lib/main.dart
@@ -17,25 +17,53 @@ class App extends StatelessWidget {
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[100],
-      appBar: AppBar(
-        leading: IconButton(
-          icon: Icon(Icons.menu),
-          tooltip: "natvigation",
-          onPressed: () => debugPrint("natvigation onPressed"),
-        ),
-        title: Text("Tobiah"),
-        elevation: 0.0,
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.search),
-            tooltip: "search",
-            onPressed: () => debugPrint("search onPressed"),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        backgroundColor: Colors.grey[100],
+        appBar: AppBar(
+          leading: IconButton(
+            icon: Icon(Icons.menu),
+            tooltip: "natvigation",
+            onPressed: () => debugPrint("natvigation onPressed"),
           ),
-        ],
+          title: Text("Tobiah"),
+          elevation: 0.0,
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.search),
+              tooltip: "search",
+              onPressed: () => debugPrint("search onPressed"),
+            ),
+          ],
+          bottom: TabBar(
+            tabs: <Widget>[
+              Tab(icon: Icon(Icons.local_florist)),
+              Tab(icon: Icon(Icons.change_history)),
+              Tab(icon: Icon(Icons.directions_bike)),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: <Widget>[
+            Icon(
+              Icons.local_florist,
+              size: 128.0,
+              color: Colors.black12,
+            ),
+            Icon(
+              Icons.change_history,
+              size: 128.0,
+              color: Colors.black12,
+            ),
+            Icon(
+              Icons.directions_bike,
+              size: 128.0,
+              color: Colors.black12,
+            ),
+          ],
+        ),
       ),
-      body: null,
     );
   }
 }
