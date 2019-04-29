db0d593dc88e5d87e1f3deb9019218623719217f
TobiahShaw <a5414994@outlook.com>
2019-04-29 08:13:00 +0800

    tab styles

--- lib/main.dart
+++ lib/main.dart
@@ -37,6 +37,10 @@ class Home extends StatelessWidget {
             ),
           ],
           bottom: TabBar(
+            unselectedLabelColor: Colors.black38,
+            indicatorColor: Colors.black54,
+            indicatorSize: TabBarIndicatorSize.label,
+            indicatorWeight: 1.0,
             tabs: <Widget>[
               Tab(icon: Icon(Icons.local_florist)),
               Tab(icon: Icon(Icons.change_history)),
