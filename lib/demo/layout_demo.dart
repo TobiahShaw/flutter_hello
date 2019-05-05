6d445b15f9fbfa1b8f9c45d0e4e36d1709563047
TobiahShaw <a5414994@outlook.com>
2019-05-05 08:18:03 +0800

    sized box widget; useage of Alignment;

--- lib/demo/layout_demo.dart
+++ lib/demo/layout_demo.dart
@@ -5,17 +5,63 @@ class LayoutDemo extends StatelessWidget {
   Widget build(BuildContext context) {
     // Row 部件内部横向排列, Column不间内部纵向排列, 借用了 css 的 flux box 的概念
     return Container(
-      child: Row(
-        // 对于 Row 和 Column 主轴是不同的 Row 主轴是横向的, Column 中主轴是纵向的
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        // 交叉轴 crossAxis 是和主轴垂直的, 可以用于该方向的对齐方式和拉伸(stretch)
-        crossAxisAlignment: CrossAxisAlignment.start,
+      child: SizedBoxDemo(),
+    );
+  }
+}
+
+class SizedBoxDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
         children: <Widget>[
-          IconBadget(Icons.pool),
-          IconBadget(Icons.beach_access, size: 64.0,),
-          IconBadget(Icons.airplanemode_active),
+          // SizedBox 不指定尺寸时，使用包裹内容的尺寸，也可以不设置内容，当作间隔来使用
+          SizedBox(
+            child: Container(
+              child: Icon(Icons.ac_unit, color: Colors.white, size: 32.0),
+              decoration: BoxDecoration(
+                  color: Color.fromRGBO(3, 53, 255, 1.0),
+                  borderRadius: BorderRadius.circular(8.0)),
+            ),
+          ),
+          SizedBox(
+            height: 16.0,
+          ),
+          SizedBox(
+            width: 200.0,
+            height: 300.0,
+            child: Container(
+              // Alignment 代表子部件的位置，0.0，0.0 代表中间，1.0，0.0代表最右边，以此类推，也可以引用系统提供的一些方式
+              alignment: Alignment(1.0, -0.8),
+              child: Icon(Icons.ac_unit, color: Colors.white, size: 32.0),
+              decoration: BoxDecoration(
+                  color: Color.fromRGBO(3, 53, 255, 1.0),
+                  borderRadius: BorderRadius.circular(8.0)),
+            ),
+          )
         ],
       ),
+    );
+  }
+}
+
+class RowOrColumnDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      // 对于 Row 和 Column 主轴是不同的 Row 主轴是横向的, Column 中主轴是纵向的
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      // 交叉轴 crossAxis 是和主轴垂直的, 可以用于该方向的对齐方式和拉伸(stretch)
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        IconBadget(Icons.pool),
+        IconBadget(
+          Icons.beach_access,
+          size: 64.0,
+        ),
+        IconBadget(Icons.airplanemode_active),
+      ],
     );
   }
 }
