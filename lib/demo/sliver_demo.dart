b6de1843262364956fabd76d9cf062933a04767e
TobiahShaw <a5414994@outlook.com>
2019-05-08 07:49:02 +0800

    usage of sliver grid

---
+++ lib/demo/sliver_demo.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:hello_world/model/post.dart';
+
+class SliverDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverGridDemo(),
+        ],
+      ),
+    );
+  }
+}
+
+class SliverGridDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SliverGrid(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 8.0,
+          mainAxisSpacing: 8.0,
+          childAspectRatio: 1.0),
+      delegate: SliverChildBuilderDelegate(
+        (BuildContext context, int index) {
+          return Container(
+            child: Image.network(
+              posts[index].imageUrl,
+              fit: BoxFit.cover,
+            ),
+          );
+        },
+        childCount: posts.length,
+      ),
+    );
+  }
+}
