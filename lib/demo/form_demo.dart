ecd96d99156cddaaae874713bcc7a9d287111961
TobiahShaw <a5414994@outlook.com>
2019-05-10 08:01:53 +0800

    usage of text field widget

--- lib/demo/form_demo.dart
+++ lib/demo/form_demo.dart
@@ -7,11 +7,60 @@ class FormDemo extends StatelessWidget {
       // 使用 Theme 部件来覆盖部分主题设置
       body: Theme(
         // 将其他主题设置 copy 过来
-        data: Theme.of(context).copyWith(
-          primaryColor: Colors.black
+        data: Theme.of(context).copyWith(primaryColor: Colors.black),
+        child: Container(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[TextFieldDemo()],
+          ),
         ),
-        child: ThemeDemo(),
       ),
+    );
+  }
+}
+
+class TextFieldDemo extends StatefulWidget {
+  @override
+  _TextFieldDemoState createState() => _TextFieldDemoState();
+}
+
+class _TextFieldDemoState extends State<TextFieldDemo> {
+  final TextEditingController textEditingController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    // 初始值
+    textEditingController.text = 'hello';
+    textEditingController
+        .addListener(() => debugPrint('input:${textEditingController.text}'));
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    // 用完销毁
+    textEditingController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(
+          icon: Icon(Icons.subject),
+          labelText: 'Tittle',
+          hintText: 'enter the title of the post.',
+          // 边框设置
+          // border: InputBorder.none,
+          // border: OutlineInputBorder(),
+          // 是否填充背景
+          filled: true,
+          // 背景色
+          fillColor: Colors.grey[200]),
+      // onChanged: (value) => debugPrint('input:$value'),
+      controller: textEditingController,
+      onSubmitted: (value) => debugPrint('submit:$value'),
     );
   }
 }
