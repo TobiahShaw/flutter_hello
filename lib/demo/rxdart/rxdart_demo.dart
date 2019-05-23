9152a8795169428c16bf0f7390972d3f7418e411
TobiahShaw <a5414994@outlook.com>
2019-05-24 07:40:34 +0800

    listen a text fielf

--- lib/demo/rxdart/rxdart_demo.dart
+++ lib/demo/rxdart/rxdart_demo.dart
@@ -20,9 +20,13 @@ class RxDartDemoHome extends StatefulWidget {
 }
 
 class _RxDartDemoHomeState extends State<RxDartDemoHome> {
+  PublishSubject<String> _textFildSubject;
   @override
   void initState() {
     super.initState();
+
+    _textFildSubject = PublishSubject<String>();
+    _textFildSubject.listen(print);
 
     // Observable<String> _observable =
     //     // Observable(Stream.fromIterable(['hello', 'Tobiah']));
@@ -36,19 +40,37 @@ class _RxDartDemoHomeState extends State<RxDartDemoHome> {
     // 特殊的subject 会把最后一次数据发射出去
     // BehaviorSubject<String> _subject = BehaviorSubject<String>();
     // 发送最后maxSize的事件给所有的监听器，无视监听时间
-    ReplaySubject<String> _subject = ReplaySubject<String>(maxSize: 2);
-    _subject.add('hi');
-    _subject.listen((data) => print('listen 1 : $data'));
-    // PublishSubject时listen2收不到消息
-    _subject.add('hello');
-    _subject.listen((data) => print('listen 2 : ${data.toUpperCase()}'));
-    _subject.add('hola');
+    // ReplaySubject<String> _subject = ReplaySubject<String>(maxSize: 2);
+    // _subject.add('hi');
+    // _subject.listen((data) => print('listen 1 : $data'));
+    // // PublishSubject时listen2收不到消息
+    // _subject.add('hello');
+    // _subject.listen((data) => print('listen 2 : ${data.toUpperCase()}'));
+    // _subject.add('hola');
 
-    _subject.close();
+    // _subject.close();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _textFildSubject.close();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Theme(
+      data: Theme.of(context).copyWith(
+        primaryColor: Colors.black,
+      ),
+      child: TextField(
+        decoration: InputDecoration(
+          labelText: 'Title',
+          filled: true,
+        ),
+        onChanged: (value) => _textFildSubject.add('input : $value'),
+        onSubmitted: (value) => _textFildSubject.add('commit : $value'),
+      ),
+    );
   }
 }
