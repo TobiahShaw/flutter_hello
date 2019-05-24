2f360b7374e38aacbf562ada1a8731f1a6efcebe
TobiahShaw <a5414994@outlook.com>
2019-05-25 07:44:52 +0800

    covert json to model

--- lib/demo/http/http_demo.dart
+++ lib/demo/http/http_demo.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+
+import 'dart:convert';
 
 class HttpDemo extends StatelessWidget {
   @override
@@ -8,6 +12,71 @@ class HttpDemo extends StatelessWidget {
         title: Text('HttpDemo'),
         elevation: 0.0,
       ),
+      body: HttpDemoHome(),
     );
   }
+}
+
+class HttpDemoHome extends StatefulWidget {
+  @override
+  _HttpDemoHomeState createState() => _HttpDemoHomeState();
+}
+
+class _HttpDemoHomeState extends State<HttpDemoHome> {
+  @override
+  void initState() {
+    super.initState();
+    fetchPost().then((value) => print(value));
+  }
+
+  Future<List<Post>> fetchPost() async {
+    final response =
+        await http.get('https://resources.ninghao.net/demo/posts.json');
+    // print('statusCode : ${response.statusCode}');
+    // print('body : ${response.body}');
+
+    if (response.statusCode == 200) {
+      final responseBody = json.decode(response.body);
+      List<Post> posts = responseBody['posts']
+          .map<Post>((item) => Post.fromJson(item))
+          .toList();
+
+      return posts;
+    } else {
+      throw Exception('Failed to fetch posts.');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class Post {
+  final int id;
+  final String title;
+  final String description;
+  final String author;
+  final String imageUrl;
+
+  Post(
+    this.id,
+    this.title,
+    this.description,
+    this.author,
+    this.imageUrl,
+  );
+
+  Post.fromJson(Map json)
+      : id = json['id'],
+        title = json['title'],
+        description = json['description'],
+        author = json['author'],
+        imageUrl = json['imageUrl'];
+
+  Map toJson() => {
+        'title': title,
+        'descritpion': description,
+      };
 }
