e97bf1ebbae9fae02a6e683007de6e8a8869b224
TobiahShaw <a5414994@outlook.com>
2019-05-26 09:20:42 +0800

    usage of AnimatedWidget;

--- lib/demo/animation/animation_demo.dart
+++ lib/demo/animation/animation_demo.dart
@@ -36,17 +36,19 @@ class _AnimationDemoHomeState extends State<AnimationDemoHome>
         // upperBound: 100.0,
         vsync: this,
         duration: Duration(milliseconds: 3000));
-    _animationController.addListener(() {
-      setState(() {});
-    });
+    // _animationController.addListener(() {
+    //   setState(() {});
+    // });
 
     _animationController.addStatusListener((status) {
       print(status);
     });
-    _curve = CurvedAnimation(parent: _animationController, curve: Curves.bounceInOut);
+    _curve = CurvedAnimation(
+        parent: _animationController, curve: Curves.bounceInOut);
     // 与直接设置lowerBound和upperBound相比更为灵活
     _animationSize = Tween(begin: 32.0, end: 100.0).animate(_curve);
-    _animationColor = ColorTween(begin: Colors.red[900], end: Colors.red).animate(_curve);
+    _animationColor =
+        ColorTween(begin: Colors.red[900], end: Colors.red).animate(_curve);
   }
 
   @override
@@ -58,28 +60,44 @@ class _AnimationDemoHomeState extends State<AnimationDemoHome>
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: IconButton(
-        icon: Icon(Icons.favorite),
-        iconSize: _animationSize.value,
-        color: _animationColor.value,
-        onPressed: () {
-          switch (_animationController.status) {
-            case AnimationStatus.forward:
-              _animationController.reverse();
-              break;
-            case AnimationStatus.completed:
-              _animationController.reverse();
-              break;
-            case AnimationStatus.reverse:
-              _animationController.forward();
-              break;
-            case AnimationStatus.dismissed:
-              _animationController.forward();
-              break;
-            default:
-          }
-        },
+      child: AnimatedHeart(
+        animations: [_animationSize, _animationColor],
+        controller: _animationController,
       ),
+    );
+  }
+}
+
+class AnimatedHeart extends AnimatedWidget {
+  final List<Animation> animations;
+  final AnimationController controller;
+
+  AnimatedHeart({this.animations, this.controller})
+      : super(listenable: controller);
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      icon: Icon(Icons.favorite),
+      iconSize: animations[0].value,
+      color: animations[1].value,
+      onPressed: () {
+        switch (controller.status) {
+          case AnimationStatus.forward:
+            controller.reverse();
+            break;
+          case AnimationStatus.completed:
+            controller.reverse();
+            break;
+          case AnimationStatus.reverse:
+            controller.forward();
+            break;
+          case AnimationStatus.dismissed:
+            controller.forward();
+            break;
+          default:
+        }
+      },
     );
   }
 }
