c4f95b9ed54199891c555f3827b42377a67b499d
TobiahShaw <a5414994@outlook.com>
2019-05-26 09:10:32 +0800

    usage Tween

--- lib/demo/animation/animation_demo.dart
+++ lib/demo/animation/animation_demo.dart
@@ -20,22 +20,32 @@ class AnimationDemoHome extends StatefulWidget {
 class _AnimationDemoHomeState extends State<AnimationDemoHome>
     with TickerProviderStateMixin {
   AnimationController _animationController;
+  Animation _animationSize;
+  Animation _animationColor;
 
   @override
   void initState() {
     super.initState();
     _animationController = AnimationController(
-        // 初始值
-        value: 32.0,
-        // 最小值
-        lowerBound: 0.0,
-        // 最大值
-        upperBound: 100.0,
+        // // 初始值
+        // value: 32.0,
+        // // 最小值
+        // lowerBound: 32.0,
+        // // 最大值
+        // upperBound: 100.0,
         vsync: this,
         duration: Duration(milliseconds: 3000));
     _animationController.addListener(() {
       setState(() {});
     });
+
+    _animationController.addStatusListener((status) {
+      print(status);
+    });
+
+    // 与直接设置lowerBound和upperBound相比更为灵活
+    _animationSize = Tween(begin: 32.0, end: 100.0).animate(_animationController);
+    _animationColor = ColorTween(begin: Colors.red[900], end: Colors.red).animate(_animationController);
   }
 
   @override
@@ -47,9 +57,27 @@ class _AnimationDemoHomeState extends State<AnimationDemoHome>
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: ActionChip(
-        label: Text('${_animationController.value}'),
-        onPressed: () => _animationController.forward(),
+      child: IconButton(
+        icon: Icon(Icons.favorite),
+        iconSize: _animationSize.value,
+        color: _animationColor.value,
+        onPressed: () {
+          switch (_animationController.status) {
+            case AnimationStatus.forward:
+              _animationController.reverse();
+              break;
+            case AnimationStatus.completed:
+              _animationController.reverse();
+              break;
+            case AnimationStatus.reverse:
+              _animationController.forward();
+              break;
+            case AnimationStatus.dismissed:
+              _animationController.forward();
+              break;
+            default:
+          }
+        },
       ),
     );
   }
