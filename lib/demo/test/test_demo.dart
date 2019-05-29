492dc91c467ac97f2631806c014cd3d837839358
TobiahShaw <a5414994@outlook.com>
2019-05-29 08:04:35 +0800

    widget testing

--- lib/demo/test/test_demo.dart
+++ lib/demo/test/test_demo.dart
@@ -8,12 +8,41 @@ class TestDemo extends StatelessWidget {
         title: Text('TestDemo'),
         elevation: 0.0,
       ),
+      body: TestDemoHome(),
+    );
+  }
+}
+
+class TestDemoHome extends StatefulWidget {
+  @override
+  _TestDemoHomeState createState() => _TestDemoHomeState();
+}
+
+class _TestDemoHomeState extends State<TestDemoHome> {
+  int count = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Chip(
+          label: Text('hello'),
+        ),
+        ActionChip(
+          label: Text('$count'),
+          onPressed: () {
+            setState(() {
+              count += 1;
+            });
+          },
+        )
+      ],
     );
   }
 }
 
 class TobiahTestDemo {
   static greet(String name) {
-    return 'hello, $name';
+    return 'hello, $name ~~';
   }
 }
--- test/tobiah_demo_test.dart
+++ test/tobiah_demo_test.dart
@@ -1,10 +1,33 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hello_world/demo/test/test_demo.dart';
 
 // flutter test test/tobiah_demo_test.dart 进行测试
 void main() {
-  test('should return hello + something.', (){
+  test('should return hello + something.', () {
     var string = TobiahTestDemo.greet('tobiah');
     expect(string, 'hello, tobiah ~~');
+  });
+
+  testWidgets('widget testing demo', (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(
+      home: TestDemo(),
+    ));
+
+    // 测试是否找到含有hello的小部件
+    final labelText = find.text('hello');
+    expect(labelText, findsNWidgets(1));
+
+    final actionChipLabelText = find.text('0');
+    expect(actionChipLabelText, findsOneWidget);
+
+    final actionChip = find.byType(ActionChip);
+    await tester.tap(actionChip);
+    // 重建一下小部件
+    await tester.pump();
+
+    final actionChipLabelTextAfterTap = find.text('1');
+    expect(actionChipLabelTextAfterTap, findsOneWidget);
+    expect(actionChipLabelText, findsNothing);
   });
 }
